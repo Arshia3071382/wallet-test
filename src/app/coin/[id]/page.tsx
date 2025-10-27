eb9eec4daed1b8308b8fac56346fba96d70960b3
Arshia <arshia.mokhtari99@gmail.com>
2025-10-27 20:35:41 +0330

    test

--- src/app/coin/[id]/page.tsx
+++ src/app/coin/[id]/page.tsx
@@ -1,28 +1,29 @@
 import { notFound } from "next/navigation";
 
-interface Coin {
-  id: string;
-  name: string;
-  price: string;
-}
-
-export default async function CoinPage({ params }: { params: { id: string } }) {
+export default async function CoinPage({ params }: any) {
   const { id } = params;
+
 
   const res = await fetch(`https://b.wallet.ir/coinlist/list`, { cache: "no-store" });
   if (!res.ok) notFound();
   const data = await res.json();
-  const coin: Coin | undefined = data.items.find((c: any) => c.currency_code === id);
+  const coin = data.items.find((c: any) => c.currency_code === id);
 
   if (!coin) notFound();
 
   return (
     <main dir="rtl" className="min-h-screen bg-gray-50 p-8">
       <h1 className="text-2xl font-bold text-blue-600 mb-4">
-        جزئیات ارز: {coin.id} - {coin.name}
+        جزئیات ارز: {coin.id} - {coin.fa_name}
       </h1>
       <p className="text-gray-600">
-        قیمت: {parseFloat(coin.price).toLocaleString()} دلار
+        قیمت دلاری: ${parseFloat(coin.price).toLocaleString()}
+      </p>
+      <p className="text-gray-600">
+        تغییر روزانه: {coin.daily_change}%
+      </p>
+      <p className="text-gray-600">
+        قیمت به تومان: {parseFloat(coin.rial_price).toLocaleString()} تومان
       </p>
     </main>
   );
